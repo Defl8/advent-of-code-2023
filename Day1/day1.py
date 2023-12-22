bad726ac3b1b9f878d6e71b953133e00834e6cc3
Wyatt James <wyattsamueljames@gmail.com>
2023-12-21 17:55:07 -0700

    All integers from the input get put into list

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,6 +1,24 @@
+from operator import truediv
+
+
 def main():
-    garbage = open("day1input.txt", "r")
-    print(garbage.readlines())
+    # Opens file containing input
+    garbage = open(
+        "C:/Users/Wyatt/Documents/github/advent-of-code-2023/Day1/input.txt", "r")
+
+# Turns input into list
+    garbage_list = garbage.readlines()
+    int_list = []
+
+# iterates over every entry in the list and adds to int list if conversion to integer is successful
+    for entry in garbage_list:
+        for c in entry:
+            try:
+                int_list.append(int(c))
+            except:
+                continue
+
+    print(int_list)
 
 
 if __name__ == "__main__":
